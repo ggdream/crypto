01c6b7f3c2d6258ab3a86c32075c4963b5b4d296
ggdream <gdream@yeah.net>
2021-01-09 00:57:09 +0800

    add docs

--- hashers.go
+++ hashers.go
@@ -11,22 +11,26 @@ import (
 
 
 
+// MD5 ...
 func MD5(text []byte) string {
 	return fmt.Sprintf("%x", md5.Sum(text))
 }
 
+// SHA256 ...
 func SHA256(text []byte) []byte {
 	hash := sha256.New()
 	hash.Write(text)
 	return hash.Sum(nil)
 }
 
+// SHA512 ...
 func SHA512(text []byte) []byte {
 	hash := sha512.New()
 	hash.Write(text)
 	return hash.Sum(nil)
 }
 
+// FileMD5 ...
 func FileMD5(file io.Reader) string {
 	hash := md5.New()
 	_, err := io.Copy(hash, file)
